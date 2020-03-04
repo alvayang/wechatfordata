d73eaf7460980d3aba95805c92da9f229bdb14eb
Kelly Cheng <wt696969@126.com>
2020-03-04 11:25:06 +0800

    add file-util

--- demo.js
+++ demo.js
@@ -4,6 +4,8 @@ const QrcodeTerminal = require('qrcode-terminal');
 const OrclUtil = require('./util/orcl-util');
 const sihuo = require('./util/sihuo');
 const BAU_schedule = require("./schedule/BAU-schedule");
+const fs = require('fs');
+const FileUtil = require('./util/file-util');
 let iniParser = require('iniparser');
 let config = iniParser.parseSync('resource/config.ini');
 
@@ -98,7 +100,7 @@ async function onMessage(msg) {
         console.log('活动查询event！');
         (async () => {
             let event_id = content.slice(6, content.length);
-            let result = await OrclUtil.executeSql('edwpool', `select * from EDW_DATA.DWL_EVENT_WEBCAST where event_id=\'${event_id}\'`)
+            let result = await OrclUtil.executeSql('edwpool', `select * from EDW_DATA.DWL_EVENT_WEBCAST where event_id=\'${event_id}\'`);
             console.log(result.rows);
             if (result.rows.length > 0) {
                 say_someting = event_id + '推送时间：' + result.rows[0][3];
@@ -113,13 +115,37 @@ async function onMessage(msg) {
         console.log('直播间房间查询event！');
         (async () => {
             let il_id = content.slice(7, content.length);
-            let result = await OrclUtil.executeSql('edwpool', `SELECT * FROM webcast.az_webcast_stg_room sr where sr.il_id=\'${il_id}\'`);
+            let result = await OrclUtil.executeSql('edwpool', `SELECT dr.room_id,
+              ROUND((((dr.end_live_time-to_date('19700101','yyyymmdd'))*86400 -
+              to_number(substr(tz_offset(sessiontimezone),1,3))*3600) -
+              ((dr.begin_live_time-to_date('19700101','yyyymmdd'))*86400 -
+              to_number(substr(tz_offset(sessiontimezone),1,3))*3600))/60,2) AS duration,
+              (SELECT count(distinct dp.pic_id) FROM webcast.az_webcast_dwh_room_picture dp where dp.il_id='66191') as pic_count，
+              dr.name,dr.begin_live_time,dr.end_live_time
+              FROM webcast.az_webcast_dwh_room dr,webcast.az_webcast_dwh_room_picture dp
+              where dr.room_id=\'${il_id}\' and rownum=1`);
             console.log(result.rows);
             if (result.rows.length > 0) {
-                say_someting = '此房间数据如下' + '：\r\nil_id=' + result.rows[0][0]
-                    + '\r\nesm_id=' + result.rows[0][1] + '\r\naz_account=' + result.rows[0][4]
-                    + '\r\nname=' + result.rows[0][3] + '\r\nbegin_live_time=' + result.rows[0][10]
-                    + '\r\nend_live_time=' + result.rows[0][8];
+                say_someting = '此房间数据如下' + '：\r\nroom_id：' + result.rows[0][0]
+                    + '\r\n时长：' + result.rows[0][1] + '\r\n照片数量：' + result.rows[0][2]
+                    + '\r\n名称：' + result.rows[0][3] + '\r\n开始时间：' + result.rows[0][4]
+                    + '\r\n结束事件：' + result.rows[0][5];
+                let audience = await OrclUtil.executeSql('edwpool', `SELECT da.room_id,
+                  NVL2(da.SSO_ID,da.SSO_ID||'-'||da.USERNAME,da.e_account_id||'-'||da.e_account_name) audience_name
+                  FROM webcast.az_webcast_dmt_audience da
+                  where da.room_id=\'${il_id}\'`);
+                if (audience.rows.length > 0) {
+                    FileUtil.delDir('files');
+                    for (let i = 0;i < audience.rows.length;i++) {
+                        fs.writeFile('files/audience_' + il_id + '.text',audience.rows[i][1],'utf8',function(err){
+                            if(err) {
+                                console.log('写文件出错了，错误是：'+err);
+                            } else {
+
+                            }
+                        });
+                    }
+                }
             } else {
                 say_someting = '无此房间数据';
             }
